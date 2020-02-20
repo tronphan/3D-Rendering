b03f5fc1c5ed73d99dd5060255218a8990ea46d0
Quy <tronphan@ems2.be>
2020-02-20 14:23:33 +0100

    Quick commit test

--- T265 Registration/T265_integration.py
+++ T265 Registration/T265_integration.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     print("1. Load point clouds")
     print("##############################################")
     # Load RGBD file from intel D435
-    pcds_file_number = range(300,310)
+    pcds_file_number = range(200, 220)
     with open("config/realsense.json") as json_file:
         config = json.load(json_file)
         initialize_config(config)
@@ -66,3 +66,36 @@ if __name__ == "__main__":
     print("Done")
     o3d.visualization.draw_geometries([pcd_combined_down])
 
+
+    print("\n")
+    print("##############################################")
+    print("4. Integrate several point cloud")
+    print("##############################################")
+    camera_poses = read_trajectory("../../TestData/RGBD/odometry.log")
+    volume = o3d.integration.ScalableTSDFVolume(
+        voxel_length=4.0 / (2*512.0),
+        sdf_trunc=0.04,
+        color_type=o3d.integration.TSDFVolumeColorType.RGB8)
+    for i in range(len(camera_poses)):
+        print("Integrate {:d}-th image into the volume.".format(i))
+        color = o3d.io.read_image(
+            "../../TestData/RGBD/color/{:05d}.jpg".format(i))
+        depth = o3d.io.read_image(
+            "../../TestData/RGBD/depth/{:05d}.png".format(i))
+        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
+            color, depth, depth_trunc=4.0, convert_rgb_to_intensity=False)
+        volume.integrate(
+            rgbd,
+            o3d.camera.PinholeCameraIntrinsic(
+                o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault),
+            np.linalg.inv(camera_poses[i].pose))
+
+    print("Extract a triangle mesh from the volume and visualize it.")
+    mesh = volume.extract_triangle_mesh()
+    mesh.compute_vertex_normals()
+    o3d.visualization.draw_geometries([mesh])
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = mesh.vertices
+    pcd.colors = mesh.vertex_colors
+    o3d.visualization.draw_geometries([pcd])
